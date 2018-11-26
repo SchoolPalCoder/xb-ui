278ddf1d9b52375d578e6bbf0e8ad0fd5cd872e7
hl <381254392@qq.com>
2018-11-26 19:57:02 +0800

    form组件

---
+++ packages/xb-form-item/src/xb-form-item.vue
@@ -0,0 +1,12 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class XbFormItem extends Vue {
+
+}
+</script>
---
+++ packages/xb-form/src/xb-form.vue
@@ -0,0 +1,12 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class XbForm extends Vue {
+
+}
+</script>
