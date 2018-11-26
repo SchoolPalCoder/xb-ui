9e6e07d22720a35e7bd03f46fdbaddc236de8a20
huanglijie <lijie.huang@xiaobao100.com>
2018-11-26 11:24:07 +0800

    删除checkbox组件（重复写了）

--- packages/xb-checkbox/index.ts
+++
@@ -1,9 +0,0 @@
-import { PluginObject } from "vue";
-import XbCheckbox from "./src/xb-checkbox.vue";
-export default XbCheckbox;
-
-export const xbCheckbox: PluginObject<XbCheckbox> = {
-  install: (Vue) => {
-    Vue.component("XbCheckbox", XbCheckbox);
-  },
-};
