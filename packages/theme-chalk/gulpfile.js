1f3bdae5fe6b48c7a5d5aad235dd0e7af085fd3a
黄莹 <ying.huang@xiaobao100.com>
2018-12-03 14:00:54 +0800

    更新代码

--- packages/theme-chalk/gulpfile.js
+++ packages/theme-chalk/gulpfile.js
@@ -1,24 +1,27 @@
 var gulp = require("gulp");
 var cleanCSS = require("gulp-clean-css"); //压缩文件大小
-var sass = require("gulp-sass");
+var less = require("gulp-less");
+const rename = require("gulp-rename");
 var autoprefixer = require("gulp-autoprefixer");
 
-// 编译sass
+// 编译less
 gulp.task("css", function() {
-    gulp.src("./src/*.scss")
-        .pipe(sass())
-        .pipe(
-            autoprefixer({
-                browsers: ["last 2 versions", "ie > 8"],
-            }),
-        )
-        .pipe(cleanCSS())
-        .pipe(gulp.dest("./dist"));
+  gulp
+    .src("./src/index.less")
+    .pipe(less())
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions", "ie > 8"],
+      })
+    )
+    .pipe(cleanCSS())
+    .pipe(rename("xbui.css"))
+    .pipe(gulp.dest("../../lib/theme-chalk"));
 });
 
 // 拷贝字体文件
 gulp.task("fonts", function() {
-    gulp.src("./src/fonts/*.*").pipe(gulp.dest("../../lib/theme-chalk/fonts"));
+  gulp.src("./src/fonts/*.*").pipe(gulp.dest("../../lib/theme-chalk/fonts"));
 });
 
 // 这里的build对应package.json中的 gulp build
--- packages/xb-text/index.ts
+++
@@ -1,9 +0,0 @@
-import { PluginObject } from "vue";
-import XbText from "./src/xb-text.vue";
-export default XbText;
-
-export const xbText: PluginObject<XbText> = {
-  install: (Vue) => {
-    Vue.component("XbText", XbText);
-  },
-};
