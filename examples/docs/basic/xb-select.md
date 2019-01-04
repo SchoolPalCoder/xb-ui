6786a5d445331a951b83e1be36b4c8751bc7d51b
黄莹 <ying.huang@xiaobao100.com>
2019-01-04 17:10:56 +0800

    基本功能已完成

---
+++ examples/docs/basic/xb-select.md
@@ -0,0 +1,248 @@
+# xb-select
+
+---
+
+## 基础用法
+
+<div class="demo-button">
+  <div>
+    <xb-select :options="option1"></xb-select>
+  </div>
+</div>
+
+```js
+<xb-select :options="option1" ></xb-select>;
+
+export default {
+  data() {
+    return {
+      option1:[{value: "这里展示的是筛选项的数据"},{value: "value1"},{value: "value2"}]
+    };
+  },
+};
+```
+
+## 修改默认文案
+
+<div class="demo-button">
+  <div>
+    <xb-select :options="option2" placeHolder="修改默认值" ></xb-select>
+  </div>
+</div>
+
+```js
+<xb-select :options="option2" placeHolder="修改默认值" ></xb-select>;
+
+export default {
+  data() {
+    return {
+      option2:[{value: "这里展示的是筛选项的数据"},{value: "value1"},{value: "value2"}]
+    };
+  },
+};
+```
+
+## 有禁用选项
+
+某一选项不允许选择
+
+<div class="demo-button">
+  <div>
+    <xb-select :options="option3" ></xb-select>
+  </div>
+</div>
+
+```js
+<xb-select :options="option3" ></xb-select>;
+
+export default {
+  data() {
+    return {
+      option3: [{value: "这里展示的是筛选项的数据",disabled:true},{value: "value1"},{value: "value2"}]
+    };
+  },
+};
+```
+
+## 禁用状态
+
+选择器不可用状态
+
+<div class="demo-button">
+  <div>
+    <xb-select :options="option4" disabled='true'></xb-select>
+  </div>
+</div>
+
+```js
+<xb-select :options="option4" disabled='true' ></xb-select>;
+
+export default {
+  data() {
+    return {
+      option4: [{value: "这里展示的是筛选项的数据"},{value: "value1"},{value: "value2"}],
+    };
+  },
+};
+```
+
+## 可清空单选
+
+包含清空按钮，可将选择器清空为初始状态
+
+<div class="demo-button">
+  <div>
+    <xb-select :options="option5" clearable="true"></xb-select>
+  </div>
+</div>
+
+```js
+<xb-select :options="option5" clearable="true" ></xb-select>;
+
+export default {
+  data() {
+    return {
+      option5:[{value: "这里展示的是筛选项的数据"},{value: "value1"},{value: "value2"}],
+    };
+  },
+};
+```
+
+## 基础多选
+
+适用性较广的基础多选，用 Tag 展示已选项
+
+<div class="demo-button">
+  <div>
+    <xb-select :options="option6" multiple='true'></xb-select>
+  </div>
+</div>
+
+```js
+<xb-select :options="option6" multiple='true' ></xb-select>;
+
+export default {
+  data() {
+    return {
+      option6: [{value: "这里展示的是筛选项的数据"},{value: "value1"},{value: "value2"}],
+    };
+  },
+};
+```
+
+## 分组
+
+备选项进行分组展示
+
+<div class="demo-button">
+  <div>
+    <xb-select :options="option7"></xb-select>
+  </div>
+</div>
+
+```js
+<xb-select :options="option7" ></xb-select>;
+
+export default {
+  data() {
+    return {
+      option7: [{
+          title: "这里是标题",
+          group:[{value: "这里展示的是筛选项的数据"},{value: "value1"},{value: "value2"}]
+        },{
+          title: "title1",
+          group:[{value: "value0"},{value: "value1"},{value: "value2"}]
+        },{
+          title: "title2",
+          group:[{value: "value0"},{value: "value1"},{value: "value2"}]
+        }]
+    };
+  },
+};
+```
+
+## 可搜索
+
+可以利用搜索功能快速查找选项
+
+<div class="demo-button">
+  <div>
+    <xb-select :options="option8" filterable="true"></xb-select>
+  </div>
+</div>
+
+```js
+<xb-select :options="option8" filterable="true"></xb-select>;
+
+export default {
+  data() {
+    return {
+      option8: [{value: "这里展示的是筛选项的数据"},{value: "value1"},{value: "value2"}],
+    };
+  },
+};
+```
+
+<script>
+  export default {
+    data() {
+      return {
+        option1: [{
+          value: '这里展示的是筛选项的数据',
+          label: '这里可以写上配置项的数据'
+        },{
+          value: 'value1'
+        },{
+          value: 'value2'
+        }],
+
+        option2: [{value: "这里展示的是筛选项的数据"},{value: "value1"},{value: "value2"},{value: "value3"}],
+
+        option3: [{value: '这里展示的是筛选项的数据',disabled:true},{value: 'value1'},{value: 'value2'},{value: "value3"}],
+
+        option4: [{value: "这里展示的是筛选项的数据"},{value: "value1"},{value: "value2"},{value: "value3"}],
+
+        option5: [{value: "这里展示的是筛选项的数据"},{value: "value1"},{value: "value2"},{value: "value3"}],
+
+        option6: [{value: "这里展示的是筛选项的数据"},{value: "value1"},{value: "value2"},{value: "value3"}],
+
+        option7: [{
+          title: "这里是标题",
+          group:[{value: "这里展示的是筛选项的数据"},{value: "value1"},{value: "value2"}]
+        },{
+          title: "title1",
+          group:[{value: "value0"},{value: "value1"},{value: "value2"}]
+        },{
+          title: "title2",
+          group:[{value: "value0"},{value: "value1"},{value: "value2"}]
+        }],
+
+        option8: [{value: "这里展示的是筛选项的数据"},{value: "value1"},{value: "value2"},{value: "value3"}],
+      }
+    }
+  }
+
+</script>
+
+## TODO
+
+分组 多选 未完成
+
+不同尺寸
+
+可搜索 分组 未完成
+
+点击旁边，关闭筛选框
+
+展示默认值
+
+## 参数表
+
+| 参数名      | 参数说明       | 类型    | 可选值 | 默认值    |
+| :---------- | :------------- | :------ | :----- | :-------- |
+| placeHolder | 输入框占位文本 | string  | ——     | 请选择... |
+| options     | 下拉框的数组   | array   | ——     | ——        |
+| disabled    | 禁用           | boolean | ——     | false     |
+| clearable   | 可清空单选     | boolean | ——     | false     |
+| multiple    | 多选           | boolean | ——     | false     |
+| filterable  | 可搜索         | boolean | ——     | false     |
---
+++ packages/xb-select/src/xb-select.vue
@@ -0,0 +1,239 @@
+<template>
+  <div style="width: 240px;">
+    <!-- 输入框 -->
+    <div :class='classesDiv' @click="toggleMenu">
+      <div :class='classesInput'>
+        <!-- 搜索框 -->
+        <input v-model="textValue" :placeholder="placeHolder" :class='classesInputShow' v-if="!valueMultiple.length" @keyup="getValue(textValue)" :readonly='readonlyShow'/>
+        <!-- 多选数据 展示 -->
+        <div style="line-height: 19px;" v-if="valueMultiple.length">
+          <span :class='classesMultiple' v-for="itemMultiple in valueMultiple">
+            <span :class='classesMultipleShow'>{{itemMultiple}}</span>
+            <i :class='classesMultipleIcon' @click="clearMultipleValue($event,itemMultiple)"></i>
+          </span>
+        </div>
+      </div>
+      <!-- icon显示 -->
+      <i :class='[classesIcon,(this.multiple && valueMultiple.length>2)?`xbui-select-icon-multiple`:``]' @click="clearValue"></i>
+    </div>
+    <!-- 筛选框 -->
+    <div :class='classesSelectDiv' v-if="toggleMenuShow">
+      <ul :class='classesSelectUl'>
+        <!-- 不分组 -->
+        <li v-for="item in selectArray" v-if="!item.group" :class='[classesSelectLi,item.disabled?classesSelectDisabled:""]' @click="toggleMenuLi(item)">
+          <span :class='classesSelectLiSpan' v-bind:style="{ textAlign: item.value=='无匹配数据'?'center':'' }">{{item.value}}</span>
+          <i :class='classesSelectLiIcon' v-if="item.isCheck"></i>
+        </li>
+        <!-- 分组 -->
+        <ul v-if="item.group&&item.group.length!=0" :class='[item.group?classesSelectLiGroup:""]' v-for="item in selectArray">
+          <li :class='[classesSelectLi,classesSelectLiGroupTitle]'>{{item.title}}</li>
+          <li :class='[itemGroup.disabled?classesSelectDisabled:"",classesSelectLi,classesSelectLiGroupContent]' v-for="itemGroup in item.group" @click="toggleMenuLi(itemGroup)">
+            <span :class='classesSelectLiSpan'>{{itemGroup.value}}</span>
+            <i :class='classesSelectLiIcon' v-if="itemGroup.isCheck"></i>
+          </li>
+        </ul>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+const prefixCls = "xbui-select";
+@Component({ name: "XbSelect" })
+export default class XbSelect extends Vue {
+  // 默认文案
+  @Prop({ default: "请选择..." })
+  placeHolder!: string;
+
+  // 数组
+  @Prop({ default: "" })
+  options!: any;
+
+  // 禁用状态
+  @Prop({ default: false })
+  disabled!: boolean;
+
+  // 可清空单选
+  @Prop({ default: false })
+  clearable!: boolean;
+
+  // 多选
+  @Prop({ default: false })
+  multiple!: boolean;
+
+  // 可搜索
+  @Prop({ default: false })
+  filterable!: boolean;
+
+  readonlyShow: boolean = !this.filterable;
+  toggleMenuShow: boolean = false;
+  textValue: string = "";
+  valueMultiple: any = [];
+  selectArray: any = this.options;
+
+  // 输入框
+  get classesDiv() {
+    return [`${prefixCls}-div`];
+  }
+  get classesInput() {
+    return [
+      `${prefixCls}-input`,
+      {
+        [`${prefixCls}-input-multiple`]: this.multiple,
+        [`${prefixCls}-input-disabled`]: this.disabled,
+      },
+    ];
+  }
+  get classesInputShow() {
+    return [
+      `${prefixCls}-input-show`,
+      {
+        [`${prefixCls}-input-disabled-show`]: this.disabled,
+      },
+    ];
+  }
+  get classesMultiple() {
+    return [`${prefixCls}-multiple`];
+  }
+  get classesMultipleShow() {
+    return [`${prefixCls}-multiple-show`];
+  }
+  get classesMultipleIcon() {
+    return [`${prefixCls}-multiple-icon`, `xb-icon-circle-close`];
+  }
+  get classesIcon() {
+    return [
+      `${prefixCls}-icon`,
+      {
+        [`xb-icon-caret-bottom`]: !this.toggleMenuShow,
+        [`xb-icon-caret-top`]: this.toggleMenuShow,
+        [`xb-icon-circle-close`]: this.clearable && this.textValue && this.toggleMenuShow,
+        [`${prefixCls}-icon-gray`]: this.clearable && this.textValue && this.toggleMenuShow,
+      },
+    ];
+  }
+  // 筛选框
+  get classesSelectDiv() {
+    return [`${prefixCls}-select-div`];
+  }
+  get classesSelectUl() {
+    return [`${prefixCls}-select-ul`];
+  }
+  get classesSelectLi() {
+    return [`${prefixCls}-select-li`];
+  }
+  get classesSelectLiSpan() {
+    return [`${prefixCls}-select-li-span`];
+  }
+  get classesSelectDisabled() {
+    return [`${prefixCls}-select-disabled`];
+  }
+  get classesSelectLiGroup() {
+    return [`${prefixCls}-select-li-group`];
+  }
+  get classesSelectLiGroupTitle() {
+    return [`${prefixCls}-select-li-group-title`];
+  }
+  get classesSelectLiGroupContent() {
+    return [`${prefixCls}-select-li-group-content`];
+  }
+  get classesSelectLiIcon() {
+    return [`${prefixCls}-select-li-icon xb-icon-check`];
+  }
+
+  // 点击展开、收缩 数据框
+  toggleMenu() {
+    if (!this.disabled) {
+      if (!this.clearable || (this.clearable && !this.toggleMenuShow)) {
+        this.toggleMenuShow = !this.toggleMenuShow;
+      }
+    }
+  }
+  // 选中一个筛选项
+  toggleMenuLi(item) {
+    if (!item.disabled) {
+      if (this.multiple) {
+        // 多选
+        if (item.isCheck) {
+          // 清除所选项
+          const indexNum = this.valueMultiple.indexOf(item.value);
+          if (indexNum !== -1) {
+            this.valueMultiple.splice(indexNum, 1);
+            item.isCheck = false;
+          }
+        } else {
+          // 添加所选项
+          this.valueMultiple.push(item.value);
+          item.isCheck = true;
+        }
+      } else {
+        // 单选
+        this.selectArray.forEach((re) => {
+          if (re.group) {
+            re.group.forEach((ele) => {
+              delete ele.isCheck;
+            });
+          } else {
+            delete re.isCheck;
+          }
+        });
+
+        this.toggleMenuShow = false;
+        this.textValue = item.value;
+        item.isCheck = true;
+      }
+      this.selectArray = this.options;
+      this.readonlyShow = true;
+    }
+  }
+  // 删除、清空 所选项
+  clearValue() {
+    if (this.textValue && this.toggleMenuShow) {
+      this.textValue = "";
+      this.selectArray.forEach((re) => {
+        delete re.isCheck;
+      });
+    }
+    this.readonlyShow = !this.filterable;
+  }
+  // 清除多选项
+  clearMultipleValue(event, item) {
+    // 结果清除选中
+    const indexNum = this.valueMultiple.indexOf(item);
+    if (indexNum !== -1) {
+      this.valueMultiple.splice(indexNum, 1);
+    }
+    // 下拉框清除选中
+    this.selectArray.forEach((re) => {
+      if (re.group) {
+        re.group.forEach((ele) => {
+          if (item === ele.value) {
+            delete ele.isCheck;
+          }
+        });
+      } else {
+        if (item === re.value) {
+          delete re.isCheck;
+        }
+      }
+    });
+    if (this.valueMultiple.length === 0) {
+      this.readonlyShow = !this.filterable;
+    }
+    event.stopPropagation();
+  }
+  // 搜索 筛选项 数据
+  getValue(value) {
+    this.selectArray = [];
+    this.options.forEach((re) => {
+      if (re.value.indexOf(value) !== -1) {
+        this.selectArray.push(re);
+      }
+    });
+    if (this.selectArray.length === 0) {
+      this.selectArray.push({ value: "无匹配数据", isValue: false });
+    }
+  }
+}
+</script>
