100f2d0c8c5cde16ae74b886bc42d35f16f6b275
SaberWang <wangshaobo8023@gmail.com>
2019-01-20 21:38:26 +0800

    打包完成

--- packages/theme-chalk/gulpfile.js
+++ packages/theme-chalk/gulpfile.js
@@ -1,28 +1,31 @@
-var gulp = require("gulp");
-var cleanCSS = require("gulp-clean-css"); //压缩文件大小
-var less = require("gulp-less");
-const rename = require("gulp-rename");
-var autoprefixer = require("gulp-autoprefixer");
+const gulp = require("gulp");
+const less = require("gulp-less");
+const cleanCss = require("gulp-clean-css");
+const autoprefixer = require("gulp-autoprefixer");
 
 // 编译less
-gulp.task("css", function() {
+gulp.task("compileCss", function() {
   gulp
-    .src("./src/index.less")
+    .src("./src/*.less")
     .pipe(less())
     .pipe(
       autoprefixer({
         browsers: ["last 2 versions", "ie > 8"],
+        cascade: false,
       })
     )
-    .pipe(cleanCSS())
-    .pipe(rename("xbui.css"))
+    // .pipe(cleanCSS())
+    .pipe(cleanCss())
     .pipe(gulp.dest("../../lib/theme-chalk"));
 });
 
 // 拷贝字体文件
-gulp.task("fonts", function() {
-  gulp.src("./src/fonts/*.*").pipe(gulp.dest("../../lib/theme-chalk/fonts"));
+gulp.task("copyFonts", function() {
+  gulp
+    .src("./src/fonts/*.*")
+    .pipe(cleanCss())
+    .pipe(gulp.dest("../../lib/theme-chalk/fonts"));
 });
 
 // 这里的build对应package.json中的 gulp build
-gulp.task("default", ["css", "fonts"]);
+gulp.task("default", ["compileCss", "copyFonts"]);
