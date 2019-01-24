4feb70d156ab342ac0ce8dae1e67cc4488db96b4
黄莹 <ying.huang@xiaobao100.com>
2019-01-24 10:27:35 +0800

    xb-tabs 基础功能

---
+++ examples/docs/navigation/xb-tabs.md
@@ -0,0 +1,79 @@
+# xb-tabs 标签页
+
+选项卡切换组件，常用于平级区域大块内容的的收纳和展现。
+
+---
+
+## 基础用法
+
+<div class="demo-button">
+  <div>
+  <xb-tabs></xb-tabs>
+    <!-- <xb-tabs value='标签一'>
+        <xb-tabs-pane label="标签一">标签一的内容</xb-tabs-pane>
+        <xb-tabs-pane label="标签二">标签二的内容</xb-tabs-pane>
+        <xb-tabs-pane label="标签三">标签三的内容</xb-tabs-pane>
+    </xb-tabs> -->
+  </div>
+</div>
+
+## 禁用某一项
+
+## 文案前加图标
+
+## 可关闭标签
+
+## 多样式展示
+
+#### 卡片样式
+
+<div class="demo-button">
+  <div>
+    <xb-tabs type='card'></xb-tabs>
+  </div>
+</div>
+
+#### 按钮样式
+
+<div class="demo-button">
+  <div>
+    <xb-tabs type='btn'></xb-tabs>
+  </div>
+</div>
+
+#### 滑块样式
+
+<div class="demo-button">
+  <div>
+    <xb-tabs type='slider'></xb-tabs>
+  </div>
+</div>
+
+## 自定义编辑文案样式
+
+可以在文案后增加未读数目
+
+## 动态编辑标签
+
+#### 在右侧增加编辑按钮
+
+#### 紧跟标签内容，增加编辑按钮
+
+## 不使用动画
+
+## 外部传入的样式
+
+#### 组件内提供的 3 种尺寸
+
+#### 自定义修改样式
+
+<script>
+  export default {
+    data() {
+      return {
+
+
+      }
+    }
+  }
+</script>
---
+++ packages/xb-tabs/src/xb-tabs.vue
@@ -0,0 +1,64 @@
+<template>
+<div>
+  <div>
+  <!-- <xb-icon type="arrow-left" size="14px"></xb-icon> -->
+  <div :class="prefixCls+'-nav'">
+    <div :class="[prefixCls+'-nav-item',prefixCls+'-nav-'+type,item.isCheck?prefixCls+'-nav-'+type+'-active':'']"  @click="handleChange(index)" v-for="(item, index) in navList">
+      <xb-icon :type="item.icon"></xb-icon>
+      <span>{{ item.name }}</span>
+    </div>
+
+  </div>
+  <!-- <xb-icon type="arrow-right" size="14px"></xb-icon> -->
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from "vue-property-decorator";
+
+@Component({ name: "XbTabs" })
+export default class XbTabs extends Vue {
+  // tab样式
+  @Prop({ default: "" })
+  value!: string;
+
+  @Prop({ default: "line" })
+  type!: string;
+
+  prefixCls = "xbui-tabs";
+  navList: any = [];
+  // navList: any = [
+  // {
+  //   isCheck: true,
+  //   icon: "edit",
+  //   name: "标题1",
+  // },
+  // {
+  //   icon: "achievement",
+  //   name: "标题2",
+  // },
+  // {
+  //   icon: "activity",
+  //   name: "标题3",
+  // },
+  // ];
+  // handleChange(index) {
+  //   this.navList.map((re) => {
+  //     delete re.isCheck;
+  //   });
+  //   this.navList[index].isCheck = true;
+  // }
+
+  mounted() {
+    // this.navList = [];
+    // console.log(this.value);
+    // let tabsPanes = this.$children.filter((item) => item.$options.name === "xb-tabs-pane");
+    // // let tabsPanes = this.$children.filter((item) => console.log(item));
+    // console.log(tabsPanes);
+  }
+}
+</script>
+
+
+
