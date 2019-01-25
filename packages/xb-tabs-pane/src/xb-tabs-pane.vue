4d2467baca639af4af155da85b24abdf36687397
黄莹 <ying.huang@xiaobao100.com>
2019-01-25 14:37:27 +0800

    鼠标移入移出时对icon的控制

--- packages/xb-tabs-pane/src/xb-tabs-pane.vue
+++ packages/xb-tabs-pane/src/xb-tabs-pane.vue
@@ -1,8 +1,8 @@
 <template>
-  <div :class="[prefixCls+'-nav-item',prefixCls+'-nav-'+parentType,isAcitve?prefixCls+'-nav-'+parentType+'-active':'',disabled?prefixCls+'-nav-item-disabled':'']">
+  <div :class="[prefixCls+'-nav-item',prefixCls+'-nav-'+parentType,isAcitve?prefixCls+'-nav-'+parentType+'-active':'',disabled?prefixCls+'-nav-item-disabled':'']" @click="handleClick()" @mouseenter="handleMouseEnter" @mouseleave="handleMouseLeave">
     <xb-icon :class="prefixCls+'-nav-vertical'" :type="icon" v-if="icon"></xb-icon>
     <span :class="prefixCls+'-nav-vertical'"><slot></slot></span>
-    <xb-icon :class="prefixCls+'-nav-vertical'" type="close" v-if="closable&&$parent.type==='card'"></xb-icon>
+    <xb-icon :class="prefixCls+'-nav-vertical'" type="close" v-if="(closable&&$parent.type==='card')&&(isAcitve||isIcon)" @click.native.stop="removeClick()"></xb-icon>
   </div>
 </template>
 
@@ -26,45 +26,65 @@ export default class XbTabsPane extends Vue {
   prefixCls = "xbui-tabs";
   parentType: string = this.$parent.type;
   isAcitve: boolean = this.label === this.$parent.value ? true : false;
+  isIcon: boolean = false;
 
-  handleChange(index) {
-    // this.$parent.value = this.label;
-    // this.$parent.$children
-    console.log(index);
-    // this.updateNav();
-    // console.log("-----------------");
-    // console.log(this.label);
-    // console.log(this.$parent.value);
-    // this.isAcitve = true;
+  // 点击切换tabs
+  handleClick() {
+    if (!this.disabled) {
+      this.$parent.handleClick(this.label);
+    }
   }
 
-  updateNav() {
+  // 移除某一个tabs
+  removeClick() {
+    if (!this.disabled) {
+      this.$parent.removeClick(this.label);
+    }
+  }
+
+  // 鼠标移入显示close的icon
+  handleMouseEnter() {
+    this.isIcon = true;
+    this.$parent.isIconShow(this.label, true);
+  }
+
+  // 鼠标移出隐藏close的icon
+  handleMouseLeave() {
+    this.isIcon = false;
+    this.$parent.isIconShow(this.label, false);
+  }
+
+  // 更新type===card的时候，下方border的位置
+  updateNavBorder() {
     if (this.parentType === "card" && this.isAcitve) {
-      this.$parent.updateNav(this.closable ? this.$el.offsetWidth + 3 : this.$el.offsetWidth, this.$el.offsetLeft);
+      this.$parent.updateNavBorder(
+        this.closable ? this.$el.offsetWidth + 3 : this.$el.offsetWidth,
+        this.$el.offsetLeft
+      );
     }
   }
 
   mounted() {
-    this.updateNav();
+    this.updateNavBorder();
   }
 
   destroyed() {
-    this.updateNav();
+    this.updateNavBorder();
   }
 
   @Watch("label")
   changeLabel(val) {
-    this.updateNav();
+    this.updateNavBorder();
   }
 
   @Watch("icon")
   changeIcon(val) {
-    this.updateNav();
+    this.updateNavBorder();
   }
 
   @Watch("disabled")
   changeDisabled(val) {
-    this.updateNav();
+    this.updateNavBorder();
   }
 }
 </script>
--- packages/xb-tabs/src/xb-tabs.vue
+++ packages/xb-tabs/src/xb-tabs.vue
@@ -9,7 +9,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue, Watch } from "vue-property-decorator";
+import { Component, Prop, Vue } from "vue-property-decorator";
 
 @Component({ name: "XbTabs" })
 export default class XbTabs extends Vue {
@@ -24,9 +24,59 @@ export default class XbTabs extends Vue {
   borderWidth: number = 0;
   borderLeft: number = 0;
 
-  updateNav(width, left) {
+  // 更新type===card的时候，下方border的位置
+  updateNavBorder(width, left) {
     this.borderWidth = width;
     this.borderLeft = left;
+  }
+
+  // 点击切换tabs
+  handleClick(label) {
+    let prevIndex = this.$children.length;
+    this.$children.forEach((re, index) => {
+      if (re.isAcitve) {
+        prevIndex = index;
+        re.isAcitve = false;
+      } else if (re.label === label) {
+        re.isAcitve = true;
+        this.borderWidth = re.$el.offsetWidth;
+        this.borderLeft =
+          prevIndex < index ? (re.closable ? re.$el.offsetLeft - 18 : re.$el.offsetLeft) : re.$el.offsetLeft;
+      }
+    });
+  }
+
+  // 移除某一个tabs
+  removeClick(label) {
+    if (this.$children.length > 1) {
+      this.$children.forEach((re, index) => {
+        if (re.label === label) {
+          if (re.isAcitve == true) {
+            // 被删除项是被选中的tab
+            let num = index + 1 == this.$children.length ? 0 : index + 1;
+            this.$children[num].isAcitve = true;
+          }
+          this.$children.splice(index, 1);
+          this.$el.getElementsByClassName("xbui-tabs-nav-item")[index].remove();
+        }
+      });
+    } else {
+      alert("没有了，不能删了");
+    }
+  }
+
+  // 显示tabs上面的close的icon
+  isIconShow(label, state) {
+    let prevIndex = this.$children.length;
+    this.$children.forEach((re, index) => {
+      if (re.isAcitve) {
+        prevIndex = index;
+      } else if (re.label === label) {
+        if (prevIndex > index) {
+          this.borderLeft = state ? this.borderLeft + 18 : this.borderLeft - 18;
+        }
+      }
+    });
   }
 }
 </script>
