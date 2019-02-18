17e629ac704f8475270898e4712eb09a1dbfcc33
黄莹 <ying.huang@xiaobao100.com>
2019-02-18 15:46:06 +0800

    xb-table 修改

--- packages/xb-table/index.ts
+++ packages/xb-table/index.ts
@@ -1,9 +1,8 @@
 import { PluginFunction } from "vue";
 import XbTable from "./src/xb-table.vue";
-export { XbTable };
+export default  XbTable ;
 
-const install: PluginFunction<XbTable> = (Vue, options) => {
+export const install: PluginFunction<XbTable> = (Vue, options) => {
   Vue.component("XbTable", XbTable);
 };
 
-export default { install };
